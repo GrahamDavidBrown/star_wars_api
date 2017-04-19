4d109960cddb28e693f159d095139b7441f43d61
Graham Brown <GrahamDavidBrown90@gmail.com>
2017-04-19 13:39:13 -0400

    heinous fix for global dirty bit, decent fix for films.

--- js/star_wars.js
+++ js/star_wars.js
@@ -1,20 +1,23 @@
 first = true;
 function render(api_address, key){
-    console.log("api_address", api_address)
-    console.log("key", key);
   $.ajax({
       type: "GET" ,
       url: api_address,
       success: function(data){
-              console.log(data.title)
-              if(first){
+              console.log(data.count)
+              if(data.count == 7 || data.count == 61 || data.count == 87 || data.count == 37 || data.count == 39){
               for(x = 0; x < data.count; x++){
                 $.ajax({
                     type: "GET" ,
                     url: api_address + x + "/",
                     success: function(data){
                     url = data.url
-                    $('div'+key).append('<div'+ data.name.replace(/\s/g, '') +'><a onclick="render(\''+url+'\',\''+data.name+'\')">' + data.name + '</a><br><//div>');
+                    if(data.name != undefined){
+                      $('div'+key).append('<div'+ data.name.replace(/\s/g, '') +'><a onclick="render(\''+url+'\',\''+data.name+'\')">' + data.name + '</a><br><//div>');
+                    }else{
+                      $('div'+key).append('<div'+ data.title.replace(/\s/g, '') +'><a onclick="render(\''+url+'\',\''+data.title+'\')">' + data.title + '</a><br><//div>');
+                    }
+
       }
     }
   )
@@ -23,7 +26,8 @@ first=false;}
             else{
               for(field in data){
                 console.log(field)
-                $('div'+data.name.replace(/\s/g, '')).append('<li>' + field +": "+ data[field] + '<br><//li>');
+                if(data.name != undefined){
+                  $('div'+data.name.replace(/\s/g, '')).append('<li>' + field +": "+ data[field] + '<br><//li>');}
           }
         }
       }
@@ -36,8 +40,7 @@ function categories(){
         type: "GET" ,
         url: 'http://swapi.co/api/',
         success: function(data){
-            for(var key in data) {
-                console.log(data)
+            for(var key in data){
                 $('body').append('<div'+ key +'><h2><a onclick="render(\''+data[key]+'\',\''+key+'\')">' + key + '</a></h2><br><//div>');
                 // $('body').append('<div'+ key +'><h2><a id='+key+'>' + key + '</a></h2><br><//div>');
                 // $("#"+ key).on('click',{api_address: data[key], key: key  }, render)
